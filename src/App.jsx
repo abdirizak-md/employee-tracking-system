869bb67eaebbc86f27824907cb6830b8f85732c8
abdirisak <abdirizakmohamed119@gamil.com>
2025-11-15 20:43:07 +0300

    header is made and finished with tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
-      <h1>hello from vite and git collabration</h1>
+      <Header />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { IoNotificationsSharp } from "react-icons/io5";
 
 function Header() {
   return (
-    <div>
-      <head>header</head>
+    <div className='bg-gray-100 flex justify-around items-center p-1'>
+      <div className='text-5xl font-extrabold'>ETS</div>
+      <div className='flex justify-around items-center'>
+        <IoNotificationsSharp size={24} className='mr-3'/>
+        <img src="./src/assets/pexels-photo.webp" alt="profile" className='w-8 h-8 mx-1 rounded-full'/>
+        <p className='font-bold text-sm mx-1'>Rizeh moha</p>
+      </div>
     </div>
   )
 }
